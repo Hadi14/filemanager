becd3381e0d6e688b640a60a9c7dfebaa108f287
Hadi14 <asusn43@yahoo.com>
2023-10-19 07:56:22 +0330

    dfg

--- scanfile.php
+++ scanfile.php
@@ -1,5 +1,13 @@
 <?php
-$dir    = __DIR__ . "/database";
-$files1 = scandir($dir);
-$files2 = scandir($dir, 1);
-print_r($files1);
+// $dir    = __DIR__ . "/database";
+// $files1 = scandir($dir);
+// $files2 = scandir($dir, 1);
+// print_r($files1);
+
+
+if (file_exists("uploads/1.pdf")) {
+    echo file_exists("uploads/1.pdf");
+    echo "oooooooooooook";
+} else {
+    echo "Nok";
+}
